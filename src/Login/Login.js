f51bd9fe79683b9df8133aaeb0330806a8a3d4e1
Douglas Gehring <douglasgehring@hotmail.com>
2020-10-13 18:02:57 -0300

    Updating mvc architecture for the login screen

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,10 +2,15 @@ import React, {useState} from 'react';
 import {Container, Input, Button, Title} from './styles'
 import FirebaseAuthAdapter from '../Data/FirebaseAuthAdapter'
 
-function LoginView({email, pass, setEmail, setPass ,signInAction, signUpAction}) {
+function LoginView({initialEmail, initialPass, signInAction, signUpAction}) {
+
+  let [email, setEmail] = React.useState(initialEmail);
+  let [pass, setPass] = React.useState(initialPass);
+
   return (
     <Container> 
       <Title>Swap Games </Title>
+      <Title>{email} {pass}</Title>
        <Input type="email" placeholder="Informe seu email"
        value={email} onChange={e=> setEmail(e.target.value)}
        />
@@ -20,13 +25,21 @@ function LoginView({email, pass, setEmail, setPass ,signInAction, signUpAction})
 
 
 export default function LoginController() {
-  
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+
+    let email = '';
+    let pass = '';
     
     const firAuthAdapter = new FirebaseAuthAdapter();
-    const signInAction = () => firAuthAdapter.signIn(email, pass);
-    const signUpAction = () => firAuthAdapter.signUp(email, pass);
+    const signInAction = () => {
+      email = 'ENTROUUU AQUI - SIGN in'
+      console.log(email, pass);
+      
+    }
+    const signUpAction = () => {
+      email = 'ENTROUUU AQUI - SIGN up'
+      console.log(email, pass);
+      firAuthAdapter.signUp(email, pass);
+    }
 
-    return LoginView(email, pass, setEmail.bind(this), setPass.bind(this), signInAction, signUpAction);
+    return LoginView(email, pass, () => signInAction, () => signUpAction);
   }
